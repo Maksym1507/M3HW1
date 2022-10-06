eb78b7cbcf8c5a0a56f71593ed99207e62c27981
Max <maksym.illarionov@nure.ua>
2022-10-06 13:33:38 +0300

    Was implemented custom list, which contains methods such as: Add, AddRange, Remove, RemoveAt and Sort. Also, was implemented iterator for custom list to iterate over the elements of the list in a foreach loop.

---
+++ M3HW1/Interfaces/IMyList.cs
@@ -0,0 +1,19 @@
+﻿namespace M3HW1.Interfaces
+{
+    public interface IMyList<T> : IEnumerable<T>
+    {
+        public int Count { get; set; }
+
+        public int Capacity { get; set; }
+
+        void Add(T item);
+
+        void AddRange(T[] collection);
+
+        bool Remove(T item);
+
+        void RemoveAt(int index);
+
+        void Sort(IComparer<T> comparer);
+    }
+}
